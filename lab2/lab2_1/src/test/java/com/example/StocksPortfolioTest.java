0e661d37c1eabfe61c080cdf77ed98d37039d2fa
tomascrl <tomasalmeida20028@gmail.com>
2023-02-28 18:36:30 +0000

    coiso

--- lab2/lab2_1/src/test/java/com/example/StocksPortfolioTest.java
+++ lab2/lab2_1/src/test/java/com/example/StocksPortfolioTest.java
@@ -1,15 +1,15 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith( MockitoExtension.class )
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class StocksPortfolioTest {
     @Mock
     private IStockMarketService stockMarket;
@@ -23,13 +23,13 @@ public class StocksPortfolioTest {
         portfolio.addStock(new Stock("IBM", 2));
         portfolio.addStock(new Stock("TSLA", 5));
 
-        Mockito.when(stockMarket.lookUpPrice("APPLE")).thenReturn(150.0);
-        Mockito.when(stockMarket.lookUpPrice("IBM")).thenReturn(130.0);
-        Mockito.when(stockMarket.lookUpPrice("TSLA")).thenReturn(200.0);
+        when(stockMarket.lookUpPrice("APPLE")).thenReturn(150.0);
+        when(stockMarket.lookUpPrice("IBM")).thenReturn(130.0);
+        when(stockMarket.lookUpPrice("TSLA")).thenReturn(200.0);
 
         double result = portfolio.getTotalValue();
 
-        assertEquals(1500.0, result, 0.0);
-        Mockito.verify(stockMarket, times(3)).lookUpPrice(anyString());
+        assertEquals(1410.0, result);
+        verify(stockMarket, times(3)).lookUpPrice(anyString());
     }
 }
